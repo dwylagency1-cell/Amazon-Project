36b4257372fe9c5e1bf817a095b18271f993b1bf
Dekiru <dwyl.agency1@gmail.com>
2026-04-01 23:11:06 +0530

    amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,4 @@
-const products = [{
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    pricecents: 1090
-},
-{
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    pricecents: 2095
-},{
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    pricecents: 799
-}];
+
 
 let productHTML = '';
 
@@ -74,7 +49,7 @@ products.forEach(function(product){
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js_add_to_cart">
             Add to Cart
           </button>
         </div>`;
@@ -83,3 +58,9 @@ products.forEach(function(product){
 
 console.log(productHTML);
 document.querySelector('.js-product-grid').innerHTML = productHTML;
+
+document.querySelectorAll('.js_add_to_cart').forEach(function(button) {
+  button.addEventListener('click', function() {
+    console.log('added')
+  })
+})
