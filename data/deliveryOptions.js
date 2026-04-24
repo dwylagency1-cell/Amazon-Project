b8894ec47b1f6fbeea33814b1e1be6ff07875d20
Dekiru <dwyl.agency1@gmail.com>
2026-04-24 16:23:08 +0530

    deliveryOptions.js

---
+++ data/deliveryOptions.js
@@ -0,0 +1,15 @@
+const deliveryOptions = [{
+    id: '1',
+    date: 7,
+    priceCents: 0
+},{
+    id: '2',
+    date: 3,
+    priceCents: 499
+},{
+    id: '3',
+    date: 1,
+    priceCents: 999
+
+}]
+
